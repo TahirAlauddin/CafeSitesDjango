40547fa4f279d3df11e843cb74309e519070a82f
tahir <tahiralauddin7@gmail.com>
2022-05-25 01:27:22 -0700

    DEBUG turned off

--- cafeDjango/settings.py
+++ cafeDjango/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECRET_KEY = "SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
    "127.0.0.1",
